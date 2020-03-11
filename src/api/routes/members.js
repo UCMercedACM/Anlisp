8429b5ab44622bfed596b0a85e179d1d19028c21
Adrian Darian <adarian@ucmerced.edu>
2020-03-10 19:36:22 -0700

    Postgres Connection Setup

--- src/api/routes/members.js
+++ src/api/routes/members.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const pool = require()
+import { query } from "../../config/postgres";
 
 /**
  * @api {get} /api/members Get Members
@@ -14,33 +14,24 @@ const pool = require()
  * @apiError (Unauthorized 401)  Unauthorized  Only authenticated users can access the data
  * @apiError (Forbidden 403)     Forbidden     Only admins can access the data
  */
-router.get("/members", async (request, response) => {
-  await response.status(200).json({
-    type: "success",
-    members: [
-      {
-        id: 100222333,
-        firstName: "TOM",
-        lastName: "Smith",
-        email: "tsmith@ucmerced.edu",
-        year: "freshmen",
-        github: "https://github.com/tsmith",
-        linkedin: "https://www.linkedin.com/in/tsmith",
-        portfolium: null,
-        handshake: null,
-        slack: "tsmith",
-        discord: "Anlisp#9248",
-        image: null,
-        active: true,
-        banned: false,
-        createdDate: new Date()
-      }
-    ]
-  });
+router.get("/member/:email", async (request, response) => {
+  db.query('SELECT * FROM MEMBERS WHERE email = $1', [request.params.email], (error, response) => {
+    if (error) {
+      response.status(404).send(error);
+    }
+
+    response.status(200).send(response.rows[0]);
+  })
 });
 
-router.post("/members", async (request, response) => {
-  await pool.query
+router.post("/members", (request, response) => {
+  query('INSERT INTO MEMBERS (student_id, first_name, last_name, email, year, github, linkedin, portfolium, handshake, slack, discord, image, active, banned) VALUES ($1, $2, $3, $4, $5, $6)', [request.body.studentId, request.body.firstName, request.body.lastName, request.body.email, request.body.year, request.body.github, request.body.linkedin, request.body.portfolium, request.body.handshake, request.body.slack, request.body.discord, request.body.image, request.body.active, request.body.banned], (error, response) => {
+    if (error) {
+      response.status(404).send(error);
+    }
+
+    response.status(200).send("Successfully inserted slides!");
+  });
 })
 
 module.exports = router;
--- src/config/postgres.js
+++ src/config/postgres.js
@@ -1,33 +1,91 @@
-import {Pool} from "pg";
-import {pg, env} from "./variables";
+import { Pool } from "pg";
+import { pg } from "./variables";
 
-const pool = new Pool();
+const pool = new Pool({
+  connectionString: pg.connectionString
+});
 
 // the pool will emit an error on behalf of any idle clients
 // it contains if a backend error or network partition happens
-pool.on('error', (err, client) => {
-  console.error('Unexpected error on idle client', err)
-  process.exit(-1)
-})
+pool.on("error", (err, client) => {
+  console.error("Unexpected error on idle client", err);
+  process.exit(-1);
+});
+
+// Setup Members table if one has not been created yet
+pool.connect().then(client => {
+  return client
+    .query(
+      `CREATE TABLE IF NOT EXISTS MEMBERS (
+        ID serial PRIMARY KEY NOT NULL,
+        student_id varchar(15) NOT NULL,
+        first_name varchar(255) NOT NULL,
+        last_name varchar(255) NOT NULL,
+        email varchar(255) NOT NULL,
+        year varchar(255),
+        github varchar(255),
+        linkedin varchar(255),
+        portfolium varchar(255),
+        handshake varchar(255),
+        slack varchar(255),
+        discord varchar(255),
+        image varchar(255),
+        active BOOLEAN NOT NULL,
+        banned BOOLEAN NOT NULL,
+        created_at TIMESTAMPTZ DEFAULT NOW()
+      );`,
+      [1]
+    )
+    .then(res => {
+      client.release();
+      console.log(res.rows[0]);
+    })
+    .catch(err => {
+      client.release();
+      console.log(err.stack);
+    });
+});
 
 /**
-* Connect to postgres db
-*
-* @returns {object} Postgres connection
-* @public
-*/
-pool
-  .connect()
-  .then(client => {
-    return client
-      .query('SELECT * FROM users WHERE id = $1', [1])
-      .then(res => {
-        client.release()
-        console.log(res.rows[0])
-      })
-      .catch(err => {
-        client.release()
-        console.log(err.stack)
-      })
-  })
-
+ * Connect to postgres db
+ *
+ * @returns {query} Postgres query
+ * @returns {getClient} Client for multiple transactions
+ * @public
+ */
+module.exports = {
+  query: (text, params, callback) => {
+    const start = Date.now();
+    return pool.query(text, params, (err, res) => {
+      const duration = Date.now() - start;
+      console.log("executed query", { text, duration, rows: res.rowCount });
+      callback(err, res);
+    });
+  },
+  getClient: callback => {
+    pool.connect((err, client, done) => {
+      const query = client.query;
+      // monkey patch the query method to keep track of the last query executed
+      client.query = (...args) => {
+        client.lastQuery = args;
+        return query.apply(client, args);
+      };
+      // set a timeout of 5 seconds, after which we will log this client's last query
+      const timeout = setTimeout(() => {
+        console.error("A client has been checked out for more than 5 seconds!");
+        console.error(
+          `The last executed query on this client was: ${client.lastQuery}`
+        );
+      }, 5000);
+      const release = err => {
+        // call the actual 'done' method, returning this client to the pool
+        done(err);
+        // clear our timeout
+        clearTimeout(timeout);
+        // set the query method back to its old un-monkey-patched version
+        client.query = query;
+      };
+      callback(err, client, release);
+    });
+  }
+};
