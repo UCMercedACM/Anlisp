4b500593a1bbdbebd95afd993d24bc33035f76bc
Adrian Darian <adarian@ucmerced.edu>
2020-03-16 23:20:32 -0700

    Setup dockerfile with proper connection to postgres database

--- src/config/variables.js
+++ src/config/variables.js
@@ -1,22 +1,25 @@
 const path = require("path");
 
 // import .env variables
-require('dotenv-safe').config({
-  path: path.join(__dirname, '../../.env'),
-  sample: path.join(__dirname, '../../.env.example'),
+require("dotenv-safe").config({
+  path: path.join(__dirname, "../../.env"),
+  sample: path.join(__dirname, "../../.env.example"),
   allowEmptyValues: true
 });
 
 module.exports = {
   env: process.env.NODE_ENV,
   port: process.env.PORT || 3000,
-  host: process.env.HOST || '0.0.0.0',
+  host: process.env.HOST || "0.0.0.0",
   jwtSecret: process.env.JWT_SECRET,
   jwtExpirationInterval: process.env.JWT_EXPIRATION_MINUTES,
   pg: {
-    connectionString: process.env.NODE_ENV === 'production' ? process.env.DATABASE_URL : `postgresql://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`,
+    connectionString:
+      process.env.NODE_ENV === "production"
+        ? process.env.POSTGRES_URL
+        : `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`
   },
-  logs: process.env.NODE_ENV === 'production' ? 'combined' : 'dev',
+  logs: process.env.NODE_ENV === "production" ? "combined" : "dev",
   rateLimitTime: process.env.RATE_LIMIT_TIME,
-  rateLimitRequest: process.env.RATE_LIMIT_REQUEST,
+  rateLimitRequest: process.env.RATE_LIMIT_REQUEST
 };
