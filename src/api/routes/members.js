8a95f2de3e07670de83a2545bd8979592384ca44
Adrian Darian <adarian@ucmerced.edu>
2020-04-01 13:39:13 -0700

    Restructured member api and added status codes, setup APIError class

--- src/api/routes/members.js
+++ src/api/routes/members.js
@@ -1,28 +1,34 @@
 const router = require("express").Router();
-const db = require("../../config/postgres");
 const jwt = require("jsonwebtoken");
-const hash = require("../../config/variables").hash;
+
+const db = require("src/config/postgres");
+const {
+  hash,
+  jwtSecret,
+  jwtExpirationInterval
+} = require("src/config/variables");
 
 /**
- * @api {get} /api/members Get Members
- * @apiDescription Returns all Members
+ * @api {get} /api/members
+ * @apiDescription Get members information
  * @apiVersion 1.0.0
- * @apiName Verify Member(s)
+ * @apiName Half-Dome
  * @apiGroup Member(s)
  * @apiPermission public
  *
- * @apiSuccess {Object[]} Returns user information.
+ * @apiSuccess {Object} Returns user information.
  *
- * @apiError (Unauthorized 401)  Unauthorized  Only authenticated users can access the data
- * @apiError (Forbidden 403)     Forbidden     Only admins can access the data
+ * @apiError  401  Unauthorized            Only authenticated users can access the data
+ * @apiError  403  Forbidden               Only admins can access the data
+ * @apiError  500  Internal Server Error   Error occurred when processing the SQL query
  */
 router.get("/member", (request, response) => {
   db.query(
     "SELECT * FROM MEMBERS WHERE email = $1",
     [request.query.email],
-    (selectError, selectResponse) => {
-      if (selectError) {
-        response.status(404);
+    (error, selectResponse) => {
+      if (error) {
+        response.status(500).send(error);
       }
 
       response.status(200).send(selectResponse);
@@ -30,35 +36,45 @@ router.get("/member", (request, response) => {
   );
 });
 
+/**
+ * @api {post} /api/login
+ * @apiDescription Authenticates a member
+ * @apiVersion 1.0.0
+ * @apiName Half Dome
+ * @apiGroup Member(s)
+ * @apiPermission public
+ *
+ * @apiSuccess {Boolean}  Returns authentication as true or false.
+ * @apiSuccess {Object}   Returns token from authentication.
+ *
+ * @apiError 401   Unauthorized            Only authenticated users can access the data
+ * @apiError 403   Forbidden               Only admins can access the data
+ * @apiError 500   Internal Server Error   Error occurred when processing the SQL query
+ */
 router.get("/login", async (request, response) => {
-  console.log(request.query);
-  var email = request.query.email;
+  // decrypt password
   hash.update(request.query.password);
-  var hashedPass = hash.digest("hex");
+  const hashedPass = hash.digest("hex");
   hash.reset();
 
-  // MAKE SURE TO: Check for valid email
+  // Check for valid email
   db.query(
     "SELECT * FROM MEMBERS WHERE email = $1",
     [request.query.email],
     (error, emailCheck) => {
       if (error) {
-        response.status(500);
+        response.status(500).send(error);
       } else if (emailCheck.rowCount == 0) {
-        response.status(200).send("Email does not exist");
+        response.status(302).send("Email does not exist");
       } else {
         if (emailCheck.rows[0].password == hashedPass) {
-          var token = jwt.sign(
-            { data: emailCheck.rows[0] },
-            "longpasswordfor31231435315checkingn92i43290stuff",
-            {
-              expiresIn: 86400 // expires in 24 hours
-            }
-          );
+          const token = jwt.sign({ data: emailCheck.rows[0] }, jwtSecret, {
+            expiresIn: jwtExpirationInterval // expires in 24 hours
+          });
           response.status(200).send({ auth: true, token: token });
         } else {
           response
-            .status(200)
+            .status(302)
             .send({ auth: false, error: "Incorrect Password" });
         }
       }
@@ -66,69 +82,102 @@ router.get("/login", async (request, response) => {
   );
 });
 
-router.post("/signup", async (request, response) => {
-  // MAKE SURE TO: Check for valid email
+/**
+ * @api {post} /api/signup
+ * @apiDescription Creates a new member
+ * @apiVersion 1.0.0
+ * @apiName Half Dome
+ * @apiGroup Member(s)
+ * @apiPermission public
+ *
+ * @apiSuccess {String} Returns success message.
+ *
+ * @apiError 401   Unauthorized            Only authenticated users can access the data
+ * @apiError 403   Forbidden               Only admins can access the data
+ * @apiError 500   Internal Server Error   Error occurred when processing the SQL query
+ */
+router.post("/signup", (request, response) => {
+  // destructure request body
+  const {
+    studentId,
+    firstName,
+    lastName,
+    email,
+    password,
+    year,
+    github,
+    linkedin,
+    personalWebsite,
+    stackOverflow,
+    portfolium,
+    handshake,
+    slack,
+    discord,
+    thumbnail
+  } = request.body;
+
+  // Check for valid email
   db.query(
-    "SELECT COUNT(email) FROM MEMBERS WHERE email = $1",
-    [request.body.email],
+    "select count(email) from members where email = $1",
+    [email],
     (error, emailCheck) => {
       if (error) {
         response.status(500);
       } else if (emailCheck.rows[0] > 0) {
-        response.status(200).send("Email already exists");
+        response.status(302).send("Email already exists");
       }
     }
   );
 
-  //VALIDATE STUDENT ID
+  // Validate student ID
   db.query(
-    "SELECT COUNT(student_id) FROM MEMBERS WHERE student_id = $1",
-    [request.body.student_id],
-    (error, stuidCheck) => {
+    "select count(student_id) from members where student_id = $1",
+    [studentId],
+    (error, studentIdCheck) => {
       if (error) {
         response.status(500);
-      } else if (stuidCheck.rows[0] > 0) {
-        response.status(200).send("Student ID already exists");
+      } else if (studentIdCheck.rows[0] > 0) {
+        response.status(302).send("Student ID already exists");
       }
     }
   );
 
-  hash.update(request.body.password);
-  var hashedPass = hash.digest("hex");
+  // hash password
+  hash.update(password);
+  const hashedPass = hash.digest("hex");
   hash.reset();
 
+  // add user to database
   db.query(
-    "insert into members (student_id, first_name, last_name, email, password, year, github, linkedin, personal_website, stack_overflow, portfolium, handshake, slack, discord, thumbnail, active, banned, privilege) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18) returning first_name;",
+    "insert into members (student_id, first_name, last_name, email, password, year, github, linkedin, personal_website, stack_overflow, portfolium, handshake, slack, discord, thumbnail, active, banned, privilege) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18);",
     [
-      request.body.studentId,
-      request.body.firstName,
-      request.body.lastName,
-      request.body.email,
+      studentId,
+      firstName,
+      lastName,
+      email,
       hashedPass,
-      request.body.year,
-      request.body.github,
-      request.body.linkedin,
-      request.body.personalWebsite,
-      request.body.stackOverflow,
-      request.body.portfolium,
-      request.body.handshake,
-      request.body.slack,
-      request.body.discord,
-      request.body.thumbnail,
-      request.body.active,
-      request.body.banned,
+      year,
+      github,
+      linkedin,
+      personalWebsite,
+      stackOverflow,
+      portfolium,
+      handshake,
+      slack,
+      discord,
+      thumbnail,
+      false,
+      false,
       "member"
     ],
-    (insertError, insertResponse) => {
+    (error, insertResponse) => {
       if (insertError) {
-        response.status(404);
+        response.status(500).send(error);
       }
 
-      console.log(insertResponse);
-
       response
-        .status(200)
-        .send(`Successfully inserted ${request.body.studentId}'s information!`);
+        .status(201)
+        .send(`Successfully inserted ${studentId}'s information!`);
     }
   );
 });
