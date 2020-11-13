a9b5d3774e51279e2f4405b39838fb79deb61634
adriandarian <adarian@ucmerced.edu>
2020-11-12 23:44:12 -0800

    fix: sign up validation

--- src/api/services/auth/auth.validation.js
+++ src/api/services/auth/auth.validation.js
@@ -1,35 +1,37 @@
 const Joi = require("@hapi/joi");
 
 module.exports = {
-  // POST /v1/auth/register
-  register: {
-    body: Joi.object({
-      email: Joi.string().email().required(),
-      password: Joi.string().required().min(6).max(128),
-    }),
-  },
+    // POST /v1/auth/register
+    register: {
+        body: Joi.object({
+            first_name: Joi.string().required(),
+            last_name: Joi.string().required(),
+            email: Joi.string().email().required(),
+            password: Joi.string().required().min(6).max(128),
+        }),
+    },
 
-  // POST /v1/auth/login
-  login: {
-    body: Joi.object({
-      email: Joi.string().email().required(),
-      password: Joi.string().required().max(128),
-    }),
-  },
+    // POST /v1/auth/login
+    login: {
+        body: Joi.object({
+            email: Joi.string().email().required(),
+            password: Joi.string().required().max(128),
+        }),
+    },
 
-  // POST /v1/auth/facebook
-  // POST /v1/auth/google
-  oAuth: {
-    body: Joi.object({
-      access_token: Joi.string().required(),
-    }),
-  },
+    // POST /v1/auth/facebook
+    // POST /v1/auth/google
+    oAuth: {
+        body: Joi.object({
+            access_token: Joi.string().required(),
+        }),
+    },
 
-  // POST /v1/auth/refresh
-  refresh: {
-    body: Joi.object({
-      email: Joi.string().email().required(),
-      refreshToken: Joi.string().required(),
-    }),
-  },
+    // POST /v1/auth/refresh
+    refresh: {
+        body: Joi.object({
+            email: Joi.string().email().required(),
+            refreshToken: Joi.string().required(),
+        }),
+    },
 };
